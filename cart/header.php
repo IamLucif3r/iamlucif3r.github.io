7f9c5fc423ec4406d74d6ff601fcbdd967d28237
Anmol Singh Yadav <53969258+IamLucif3r@users.noreply.github.com>
2021-01-08 18:04:50 +0000

    added "Shopping Cart Using PHP" Project

---
+++ cart/header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&family=Ubuntu&display=swap" rel="stylesheet"> 
+    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Shopping</title>
+   
+    <style>
+        body{
+        
+           font-family: 'Ubuntu', sans-serif;
+        
+        }
+        
+        li{
+            display: inline;
+            margin: 20px;
+            padding:20px;
+        }
+        a{
+            color: red;
+           text-decoration: none;
+        }
+        a:hover{
+            color: yellowgreen ;
+        }
+        </style>
+</head>
+<body>
+    <li> <a href="index.php"> Home </a></li>
+    <li> <a href="mycart.php"> MyCart</a> </li>
+
+
+
+</body>
+</html>
